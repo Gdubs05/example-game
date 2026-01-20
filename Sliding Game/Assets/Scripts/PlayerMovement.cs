fd9a14cf1ab9637e7ae852cc64fd811b0caaac84
Gdubs05 <gwesleyking@gmail.com>
2026-01-20 16:52:31 -0600

    Restrict Player rotation with Physics material
    Optimize the force being added to the player object by creating a public variable

--- Sliding Game/Assets/Scripts/PlayerMovement.cs
+++ Sliding Game/Assets/Scripts/PlayerMovement.cs
@@ -4,10 +4,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
 
+    public float forwardForce = 2000f;
   
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime); //Apply a force of 2000 to the z-axis
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime); //Apply a force to the z-axis
     }
 }
